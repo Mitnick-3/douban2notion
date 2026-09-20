44dda01bc3ca74257f1c57e6c7cc9b42c298419a
Mitnick-3 <53416267+Mitnick-3@users.noreply.github.com>
2026-09-20 11:19:44 +0800

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -71,7 +71,7 @@ def get_file(url):
 
 
 def get_multi_select(names):
-    return {"multi_select": [{"name": name} for name in names]}
+    return {"RICH_TEXT": [{"name": name} for name in names]}
 
 
 def get_relation(ids):
