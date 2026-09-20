d68ae9f43541a0aa77a0bfa06f24b13b89241d86
Mitnick-3 <53416267+Mitnick-3@users.noreply.github.com>
2026-09-20 11:46:32 +0800

    Update douban.py

--- scripts/douban.py
+++ scripts/douban.py
@@ -145,34 +145,33 @@ def insert_movie():
         if subject.get("actors"):
             l = []
             actors = subject.get("actors")[0:100]
-        for actor in actors:
-            if actor.get("name"):
-                if "/" in actor.get("name"):
-                    l.extend(actor.get("name").split("/"))
-                else:
-                    l.append(actor.get("name"))
-    # 把列表拼接成中文逗号分隔字符串
-     actor_str = "，".join(l)
-     movie["演员"] = {
-            "rich_text": [
-                {
-                    "text": {
-                        "content": actor_str
+            for actor in actors:
+                if actor.get("name"):
+                    if "/" in actor.get("name"):
+                        l.extend(actor.get("name").split("/"))
+                    else:
+                        l.append(actor.get("name"))
+            actor_str = "，".join(l)
+            movie["演员"] = {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": actor_str
+                        }
+                        }
+                ]
+            }
+        else:
+    # 没有演员数据时，设置空富文本，避免接口报错
+            movie["演员"] = {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": ""
+                        }
                     }
-                }
-            ]
-        }
-    else:
-    # 没有演员时，给空富文本，防止报错
-        movie["演员"] = {
-            "rich_text": [
-                {
-                    "text": {
-                        "content": ""
-                    }
-                }
-            ]
-        }
+                ]
+            }
             if subject.get("directors"):
                 movie["导演"] = [
                     notion_helper.get_relation_id(
